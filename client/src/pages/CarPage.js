47e20e452dcc169dd84a8a2e425e36a521685f75
Kaynã Mesquita <mesquitakayna@gmail.com>
2024-10-20 11:16:23 -0300

    fetch car page data from api

--- client/src/pages/CarPage.js
+++ client/src/pages/CarPage.js
@@ -1,21 +1,40 @@
 import "../style-car-page.css";
-import { Component } from "react";
+import React, { Component, useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import Popup from 'reactjs-popup';
 import logo from '../logo-stego.png';
 
-export class CarPage extends Component {
-  componentDidMount() {
-    
-  }
-  render() {
-    return (
-      <>
-        <div className="container__carpage">
-          <header>
-            <nav>
-              <div class="logo">
-                <a href="#"><img src={logo} alt="" /></a>
-              </div>
+export const CarPage = () => {
+  const [data, setData] = useState({images: [], seller: {}});
+  const { id } = useParams();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('/api/vehicles/' + id);
+        const result = await response.json();
+        result.price = result.price.toLocaleString('pt-BR');
+        
+        const response_user = await fetch('/api/users/' + result.seller_id);
+        const result_user = await response_user.json();
+        setData({...result, seller: {...result_user}});
+
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      <div className="container__carpage">
+        <header>
+          <nav>
+            <div class="logo">
+              <a href="#"><img src={logo} alt="" /></a>
+            </div>
 
               <div class="options">
                 <button class="op">
@@ -223,111 +242,109 @@ export class CarPage extends Component {
                 </button>
               </div>
 
-              <Popup
-                trigger={<button class="account-circle"><span class="material-symbols-outlined">account_circle</span></button>}
-                modal
-                contentStyle={{
-                  position: "fixed",
-                  top: "50%",
-                  left: "50%",
-                  transform: "translate(-50%, -50%)",
-                  width: "600px",
-                  padding: "40px",
-                  boxShadow: "0px 0px 10px 6px #a1a1a150",
+            <Popup
+              trigger={<button class="account-circle"><span class="material-symbols-outlined">account_circle</span></button>}
+              modal
+              contentStyle={{
+                position: "fixed",
+                top: "50%",
+                left: "50%",
+                transform: "translate(-50%, -50%)",
+                width: "600px",
+                padding: "40px",
+                boxShadow: "0px 0px 10px 6px #a1a1a150",
+                border: "none",
+                backgroundColor: "#fff",
+                borderRadius: "10px"
+              }}
+              overlayStyle={{ background: "rgba(0, 0, 0, 0.5)" }}
+            >
+              <div style={{ paddingBottom: "20px", textAlign: "center" }}>
+                <h1>Meu Perfil</h1>
+              </div>
+              <div style={{ color: "#a1a1a1" }}>
+                <p style={{ padding: "10px 0px" }}><span style={{ color: "#000" }}>Nome: </span>Breno</p>
+                <p style={{ padding: "10px 0px" }}><span style={{ color: "#000" }}>CPF: </span>999.999.99-99</p>
+                <p style={{ padding: "10px 0px" }}><span style={{ color: "#000" }}>Email: </span>breno@gmail.com</p>
+                <p style={{ padding: "10px 0px" }}><span style={{ color: "#000" }}>Telefone: </span>(84) 99999-9999</p>
+              </div>
+              <div style={{ textAlign: "center" }}>
+                <h3 style={{ padding: "30px 0px 20px 0px" }}>Veículos Anunciados</h3>
+                <a href="car-page.html" style={{ textDecoration: "none", color: "#000" }}>
+                  <p style={{ fontSize: "15px", fontWeight: "600", padding: "10px 0px" }}>LAND ROVER <span style={{ color: "#f6302f" }}>RANGE ROVER VELAR</span></p>
+                </a>
+              </div>
+              <div style={{ display: "flex", justifyContent: "center", alignItems: "center", gap: "20px", paddingTop: "40px" }}>
+                <button style={{
+                  width: "200px",
+                  padding: "8px",
                   border: "none",
-                  backgroundColor: "#fff",
-                  borderRadius: "10px"
-                }}
-                overlayStyle={{ background: "rgba(0, 0, 0, 0.5)" }}
-              >
-                <div style={{ paddingBottom: "20px", textAlign: "center" }}>
-                  <h1>Meu Perfil</h1>
-                </div>
-                <div style={{ color: "#a1a1a1" }}>
-                  <p style={{ padding: "10px 0px" }}><span style={{ color: "#000" }}>Nome: </span>Breno</p>
-                  <p style={{ padding: "10px 0px" }}><span style={{ color: "#000" }}>CPF: </span>999.999.99-99</p>
-                  <p style={{ padding: "10px 0px" }}><span style={{ color: "#000" }}>Email: </span>breno@gmail.com</p>
-                  <p style={{ padding: "10px 0px" }}><span style={{ color: "#000" }}>Telefone: </span>(84) 99999-9999</p>
-                </div>
-                <div style={{ textAlign: "center" }}>
-                  <h3 style={{ padding: "30px 0px 20px 0px" }}>Veículos Anunciados</h3>
-                  <a href="car-page.html" style={{ textDecoration: "none", color: "#000" }}>
-                    <p style={{ fontSize: "15px", fontWeight: "600", padding: "10px 0px" }}>LAND ROVER <span style={{ color: "#f6302f" }}>RANGE ROVER VELAR</span></p>
-                  </a>
-                </div>
-                <div style={{ display: "flex", justifyContent: "center", alignItems: "center", gap: "20px", paddingTop: "40px" }}>
-                  <button style={{
-                    width: "200px",
-                    padding: "8px",
-                    border: "none",
-                    fontWeight: "bolder",
-                    fontSize: "16px",
-                    borderRadius: "6px",
-                    backgroundColor: "#f6302f",
-                    color: "#fff",
-                    cursor: "pointer"
-                  }}>OK</button>
-                  <button style={{
-                    width: "200px",
-                    padding: "8px",
-                    border: "none",
-                    fontWeight: "bolder",
-                    fontSize: "16px",
-                    borderRadius: "6px",
-                    backgroundColor: "#f6302f",
-                    color: "#fff",
-                    cursor: "pointer"
-                  }}>Gerar PDF</button>
-                </div>
-              </Popup>
-            </nav>
-          </header>
+                  fontWeight: "bolder",
+                  fontSize: "16px",
+                  borderRadius: "6px",
+                  backgroundColor: "#f6302f",
+                  color: "#fff",
+                  cursor: "pointer"
+                }}>OK</button>
+                <button style={{
+                  width: "200px",
+                  padding: "8px",
+                  border: "none",
+                  fontWeight: "bolder",
+                  fontSize: "16px",
+                  borderRadius: "6px",
+                  backgroundColor: "#f6302f",
+                  color: "#fff",
+                  cursor: "pointer"
+                }}>Gerar PDF</button>
+              </div>
+            </Popup>
+          </nav>
+        </header>
 
-          <main>
-            <section>
-              <div class="carousel-container" id="carouselContainer">
-                <img src="https://image.webmotors.com.br/_fotos/anunciousados/gigante/2024/202410/20241003/land-rover-range-rover-velar-2-0-p300-gasolina-rdynamic-se-automatico-wmimagem22333765963.webp?s=fill&w=552&h=414&q=60" alt="Carro 1" class="carousel-image" />
-                <img src="https://image.webmotors.com.br/_fotos/anunciousados/gigante/2024/202410/20241003/land-rover-range-rover-velar-2-0-p300-gasolina-rdynamic-se-automatico-wmimagem22333800235.webp?s=fill&w=1920&h=1440&q=75" alt="Carro 2" class="carousel-image" />
-                <img src="https://image.webmotors.com.br/_fotos/anunciousados/gigante/2024/202410/20241003/land-rover-range-rover-velar-2-0-p300-gasolina-rdynamic-se-automatico-wmimagem22333904878.webp?s=fill&w=1920&h=1440&q=75" alt="Carro 3" class="carousel-image" />
-              </div>
+        <main>
+          <section>
+            <div class="carousel-container" id="carouselContainer">
+            {data.images.map((image) => (
+              <img src={image} alt="Carro 1" class="carousel-image" />  
+            ))}
+            </div>
+            <div class="navigation-buttons">
+              <button id="prevBtn">&#10094;</button>
+              <button id="nextBtn">&#10095;</button>
+            </div>
 
-              <div class="navigation-buttons">
-                <button id="prevBtn">&#10094;</button>
-                <button id="nextBtn">&#10095;</button>
+            <div class="fullscreen-img" id="fullscreenImg">
+              <span class="close" id="closeFullscreen">&times;</span>
+              <img id="fullscreenImage" src="" alt="Fullscreen Image" />
+            </div>
+          </section>
+          <section class="car-information">
+            <div class="specifications">
+              <div class="car-title">
+                <p>{data.brand} <span class="color">{data.model}</span></p>
               </div>
-
-              <div class="fullscreen-img" id="fullscreenImg">
-                <span class="close" id="closeFullscreen">&times;</span>
-                <img id="fullscreenImage" src="" alt="Fullscreen Image" />
+              <div class="informations">
+                <p><span>ano</span>{data.year}</p>
+                <p><span>tipo</span>{data.type}</p>
+                <p><span>km</span>{data.mileage}</p>
+                <p><span>câmbio</span>{data.transmission}</p>
+                <p><span>combustivel</span>{data.fuel_type}</p>
+                <p><span>motor</span>{data.engine}</p>
               </div>
-            </section>
-            <section class="car-information">
-              <div class="specifications">
-                <div class="car-title">
-                  <p>LAND ROVER <span class="color">RANGE ROVER VELAR</span></p>
-                </div>
-                <div class="informations">
-                  <p><span>ano</span>2020</p>
-                  <p><span>tipo</span>carro</p>
-                  <p><span>km</span>58.000</p>
-                  <p><span>câmbio</span>automatico</p>
-                  <p><span>combustivel</span>flex</p>
-                  <p><span>motor</span>2.0</p>
-                </div>
+            </div>
+            <div class="value-and-contact">
+              <div class="value">
+                <h1>R$ {data.price}</h1>
               </div>
-              <div class="value-and-contact">
-                <div class="value">
-                  <h1>R$ 355.000</h1>
-                </div>
-                <div class="contact">
-                  <h4>Entre em contato</h4>
-                  <p>(84) 99641-4098</p>
-                </div>
+              <div class="contact">
+                <h4>Entre em contato</h4>
+                <p>{data.seller.phone_number}</p>
               </div>
-            </section>
-          </main>
-        </div>
-      </>
-    );
-  }
+            </div>
+          </section>
+        </main>
+      </div>
+    </>
+  );
 }
